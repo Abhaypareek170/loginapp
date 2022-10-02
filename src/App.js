af14872dfbf33f9b906d7e1e961228400bb93299
Abhay Pareek <abhaypareek170@gmail.com>
2022-10-02 17:34:44 +0530

    Adding files

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  
+  useEffect(() => {
+    const userLogInInfo = localStorage.getItem("isLoggedIn");
+      if(userLogInInfo==='1'){
+        setIsLoggedIn(true);
+      }
+    },[])
+  
+  const loginHandler = (email, password ,college) => {
+    localStorage.setItem("isLoggedIn","1")
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+    localStorage.removeItem("")
+  };
+
+  return (
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </React.Fragment>
+  );
+}
+
+export default App;
